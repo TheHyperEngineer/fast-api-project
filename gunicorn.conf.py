7faafd481922df2482dfc75431b86d5fe0665677
Hyper Engineer <admin-user@hyper-engineer.com>
2025-11-26 18:05:36 -0600

    updates to yaml file

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,9 @@
 import multiprocessing
-from pythonjsonlogger import jsonlogger
+try:
+    from pythonjsonlogger import jsonlogger  # type: ignore
+    JSONLOGGER_AVAILABLE = True
+except Exception:
+    JSONLOGGER_AVAILABLE = False
 
 worker_processes = max(2, multiprocessing.cpu_count() * 2 + 1)
 bind = "0.0.0.0:8000"
@@ -10,23 +14,39 @@ errorlog = "-"
 loglevel = "info"
 
 # Configure JSON formatted logs for the Gunicorn process
-logconfig_dict = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'json': {
-            '()': 'pythonjsonlogger.jsonlogger.JsonFormatter',
-            'fmt': '%(asctime)s %(name)s %(levelname)s %(message)s'
+if JSONLOGGER_AVAILABLE:
+    logconfig_dict = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'json': {
+                # A string import target (dictConfig) will try to import the class at runtime
+                '()': 'pythonjsonlogger.jsonlogger.JsonFormatter' if JSONLOGGER_AVAILABLE else 'logging.Formatter',
+                'fmt': '%(asctime)s %(name)s %(levelname)s %(message)s'
+            }
+        },
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'json'
+            }
+        },
+        'root': {
+            'level': 'INFO',
+            'handlers': ['console']
+        },
+        'loggers': {
+            'gunicorn.error': {
+                'level': 'INFO',
+                'handlers': ['console'],
+                'propagate': False
+            },
+            'gunicorn.access': {
+                'level': 'INFO',
+                'handlers': ['console'],
+                'propagate': False
+            }
         }
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'json'
-        }
-    },
-    'root': {
-        'level': 'INFO',
-        'handlers': ['console']
     }
-}
+# Do not use a complex dictLogConfig for Gunicorn here; keep default access/error logs to STDOUT
+logconfig_dict = None
